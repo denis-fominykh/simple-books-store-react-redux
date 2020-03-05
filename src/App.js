8891db9d5b3d9a603d02f8b3be362f323b8cb43e
denis-fominykh <fominykhdenis@gmail.com>
2020-03-05 23:02:32 +0200

    starting a project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Simple Books Store</h1>
+    </div>
+  );
+};
+
+export default App;

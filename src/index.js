33060dccd46644d47836dd9c4a9689caa53c6df5
denis-fominykh <fominykhdenis@gmail.com>
2020-03-06 13:51:23 +0200

    adding the wireframe to an application

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import ErrorBoundry from './components/ErrorBoundry/ErrorBoundry';
+import BookstoreService from './services/bookstoreService';
+import { BookstoreServiceProvider } from './components/BookstoreServiceContext/BookstoreServiceContext';
+
+import store from './store';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const bookstoreService = new BookstoreService();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ErrorBoundry>
+      <BookstoreServiceProvider value={bookstoreService}>
+        <Router>
+          <App/>
+        </Router>
+      </BookstoreServiceProvider>
+    </ErrorBoundry>
+  </Provider>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();

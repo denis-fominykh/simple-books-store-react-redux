1e5e8f3e7535b64f5c8b53f49c864389339c4d95
denis-fominykh <fominykhdenis@gmail.com>
2020-03-06 17:26:56 +0200

    displaying the books on a home page

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import BookList from '../BookList/BookList';
 
 const Home = () => {
-  return <div>Home Page</div>;
+  const books = [
+    {
+      id: 1,
+      title: "The Captain's Daughter",
+      author: 'Alexander Pushkin',
+    },
+    {
+      id: 2,
+      title: 'The Call of the Wild',
+      author: 'Jack London',
+    },
+  ]
+  return (
+    <BookList books={books}/>
+  );
 }
 
 export default Home;

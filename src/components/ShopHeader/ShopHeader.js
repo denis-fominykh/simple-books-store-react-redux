d8a0552805c96bd1d74cabf54a2f3c907efa89ff
denis-fominykh <fominykhdenis@gmail.com>
2020-03-08 00:05:58 +0200

    creating the shop header styles

--- src/components/ShopHeader/ShopHeader.js
+++ src/components/ShopHeader/ShopHeader.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './ShopHeader.scss';
 
 const ShopHeader = ({ numItems, total }) => {
   return (
     <header className="shop-header row">
       <a className="logo text-dark" href="/#">Simple Books Store</a>
-      <a href="/#">
-        <i className="cart-icon fa fa-shoping-cart" />
+      <a className="shopping-cart" href="/#">
+        <i className="cart-icon fa fa-shopping-cart" />
         {numItems} items (${total})
       </a>
     </header>

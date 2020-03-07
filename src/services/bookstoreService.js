2095dfaf78a010bd264ebb0e04312266940a9e11
denis-fominykh <fominykhdenis@gmail.com>
2020-03-07 23:37:06 +0200

    adding more data to the bookstore service

--- src/services/bookstoreService.js
+++ src/services/bookstoreService.js
@@ -5,11 +5,15 @@ export default class BookstoreService {
         id: 1,
         title: "The Captain's Daughter",
         author: 'Alexander Pushkin',
+        price: 15,
+        coverImage: 'https://images-na.ssl-images-amazon.com/images/I/51y197Q4HTL.jpg',
       },
       {
         id: 2,
         title: 'The Call of the Wild',
         author: 'Jack London',
+        price: 13,
+        coverImage: 'https://images-eu.ssl-images-amazon.com/images/I/51NOAJhXMRL.jpg',
       },
     ];
   }

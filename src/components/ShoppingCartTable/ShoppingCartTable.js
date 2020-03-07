3c5fbdd190a341ca54940ec279f5e22b3d690987
denis-fominykh <fominykhdenis@gmail.com>
2020-03-08 00:29:22 +0200

    creating the shopping cart table component

--- src/components/ShoppingCartTable/ShoppingCartTable.js
+++ src/components/ShoppingCartTable/ShoppingCartTable.js
@@ -4,6 +4,37 @@ const ShoppingCartTable = () => {
   return (
     <div className="shopping-cart-table">
       <h2>Your Order</h2>
+      <table className="table">
+        <thead>
+          <th>#</th>
+          <th>Item</th>
+          <th>Count</th>
+          <th>Price</th>
+          <th>Action</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Other</td>
+            <td>2</td>
+            <td>$12</td>
+            <td>
+              <button className="btn btn-outline-danger btn-sm float-right">
+                <i className="fa fa-trash" />
+              </button>
+              <button className="btn btn-outline-success btn-sm float-right">
+                <i className="fa fa-plus-circle" />
+              </button>
+              <button className="btn btn-outline-warning btn-sm float-right">
+                <i className="fa fa-minus-circle" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="total">
+        Total: $20
+      </div>
     </div>
   )
 }

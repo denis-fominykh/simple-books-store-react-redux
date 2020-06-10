76c4d1085c7a28af6e361a7973cac3ec39610ea0
denis-fominykh <fominykhdenis@gmail.com>
2020-06-10 18:17:40 +0300

    adding the links

--- src/components/ShopHeader/ShopHeader.js
+++ src/components/ShopHeader/ShopHeader.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './ShopHeader.scss';
 
 const ShopHeader = ({ numItems, total }) => {
   return (
     <header className="shop-header row">
-      <a className="logo text-dark" href="/#">
-        Simple Books Store
-      </a>
-      <a className="shopping-cart" href="/#">
-        <i className="cart-icon fa fa-shopping-cart" />
-        {numItems} items (${total})
-      </a>
+      <Link to="/#">
+        <div className="logo text-dark">Simple Books Store</div>
+      </Link>
+      <Link to="/card">
+        <div className="shopping-cart">
+          <i className="cart-icon fa fa-shopping-cart" />
+          {numItems} items (${total})
+        </div>
+      </Link>
     </header>
   );
 };

0a3518ae077276ac3fb3e68daa4f46855cf811ef
denis-fominykh <fominykhdenis@gmail.com>
2020-06-10 17:50:37 +0300

    creating a promise into bookstoreservice

--- src/services/bookstoreService.js
+++ src/services/bookstoreService.js
@@ -1,22 +1,30 @@
-export default class BookstoreService {
-  getBooks() {
-    return [
-      {
-        id: 1,
-        title: "The Captain's Daughter",
-        author: 'Alexander Pushkin',
-        price: 15,
-        coverImage:
-          'https://images-na.ssl-images-amazon.com/images/I/51y197Q4HTL.jpg',
-      },
-      {
-        id: 2,
-        title: 'The Call of the Wild',
-        author: 'Jack London',
-        price: 13,
-        coverImage:
-          'https://images-eu.ssl-images-amazon.com/images/I/51NOAJhXMRL.jpg',
-      },
-    ];
-  }
+class BookstoreService {
+  data = [
+    {
+      id: 1,
+      title: "The Captain's Daughter",
+      author: 'Alexander Pushkin',
+      price: 15,
+      coverImage:
+        'https://images-na.ssl-images-amazon.com/images/I/51y197Q4HTL.jpg',
+    },
+    {
+      id: 2,
+      title: 'The Call of the Wild',
+      author: 'Jack London',
+      price: 13,
+      coverImage:
+        'https://images-eu.ssl-images-amazon.com/images/I/51NOAJhXMRL.jpg',
+    },
+  ];
+
+  getBooks = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.data);
+      }, 700);
+    });
+  };
 }
+
+export default BookstoreService;
